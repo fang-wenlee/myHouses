ed51c5ce5fddb08cda96898387819866419c7d13
Fang-Wen Lee <fangwenlee@hotmail.com>
2020-12-14 17:16:51 -0800

    initialize repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# this is the project README file
---
+++ js/design.js
@@ -0,0 +1,62 @@
+/* JavaScript Document */
+
+$(document).ready(function(){
+
+  $.getJSON('data.json', function(data) {
+
+  /*  var output='';
+        $.each(data, function(key, val) {
+            output += '<aside><div class="content' +val.hid+ '">';
+            output += '<h3><a href=#>'+ val.street + "," + val.city +'</a></h3>';
+            output += '<img src="../images/'+ val.hid +'V1L.jpeg" alt="'+ val.street +'" />';
+            output += '</div></aside>';
+        });*/
+    //output += '</aside>';
+   var output='';
+          $.each(data, function(key, val) {
+              output += '<aside><a href="http://www.baytour.com/Nvt/index.aspx?hid=';
+              output += val.hid + '&m=13" target="_blank" ><div class="posts-img" ';
+              output += ' style="background:url(../images/'+val.hid +'V1L.jpeg); background-size:cover; background-position:center;">';
+              output += '</div></a>';
+              output += '<div class="posts-detail"><h3><a href=#>'+ val.street + "," + val.city +'</a></h3>';
+              output += '</div>';
+              output += '</aside>';
+          });
+    /*      var output='';
+                  $.each(data, function(key, val) {
+                      output += '<aside><div class="container" style="background:url(../images/'+val.hid +'V1L.jpeg)">';
+                    //  output += '<h3><a href=#>'+ val.street + "," + val.city +'</a></h3>';
+                      output += '</div></aside>';
+  output += '<div class="detail"> <h3><a href=#>'+ val.street + "," + val.city +'</a></h3>';
+                      output += '</div></aside>';
+                  }); */
+
+    $('section.main .house-container').append(output);
+  }); //get JSON
+
+
+     $('nav a.mobile_menu').on('click', function(){
+
+          var currentNavHeight = $('nav').height();
+
+          if (currentNavHeight < 5){
+              var newNavHeight = $('nav >ul').height() + 25;
+               $('nav').animate({'height' : newNavHeight+ 'px'}, 750 )
+          }
+          else
+          {
+              $('nav').animate({'height' : '0px'}, 750, function(){
+                $(this).removeAttr('style');
+              })}//else
+
+
+     });
+
+    // $(window).resize(function(){
+    //   if ($(this).width() > 625){
+    //     $('nav').removeAttr('style'); }
+    // })
+
+
+
+});//ready
